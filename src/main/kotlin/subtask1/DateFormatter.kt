1a0ef098b6eaf423bbd298bbeb397c16cd9d6aa9
MartynovMaksim <martynmaks@gmail.com>
2021-06-11 21:27:28 +0300

    complete version 1.01

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -10,12 +10,13 @@ class DateFormatter {
         calendar.set(Calendar.DAY_OF_MONTH, day.toInt())
         calendar.set(Calendar.MONTH, month.toInt() - 1)
         calendar.set(Calendar.YEAR,year.toInt())
-        val dateFormat = SimpleDateFormat("d MMMM, EEEE")
-        if (day.toInt() !in 1..31 || month.toInt() !in 1..12) return "Такого дня не существует"
-        if (day.toInt() == 29 && month.toInt() == 2) {
-            return if (calendar.isLeapYear(year.toInt())) dateFormat.format(calendar.time)
-            else "Такого дня не существует"
+        val locale = Locale("ru")
+        val dateFormat = SimpleDateFormat("d MMMM, EEEE",locale)
+        calendar.isLenient = false
+        return try {
+            dateFormat.format(calendar.time)
+        } catch (e: Exception) {
+            "Такого дня не существует"
         }
-        return dateFormat.format(calendar.time)
     }
 }
